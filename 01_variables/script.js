70d30d5e66454107868dc20fe49bc7dacfdb1ade
Nandan Patil <150545490+nandangitdesk@users.noreply.github.com>
2024-01-21 01:47:15 +0530

    learn javascript variables and datatypes

---
+++ 01_variables/script.js
@@ -0,0 +1,39 @@
+const accountNumber = 309330;
+console.log(accountNumber);
+
+// accountNumber = 123456
+// console.log(accountNumber);
+//this is not allowed ,constant is declared only one time
+
+let accountEmail = "abc@gmail.com";
+console.log(accountEmail);
+
+//we can modified let memory further
+//variable name = memory
+
+var accountName = "nandan";
+console.log(accountName);
+
+
+// don't use var bcz modern developer doesn't follow this practice,
+// or there is issue in javascript of BLOCK SCOPE 
+
+
+
+accountState = "active";
+//we can defined this type of memory also but this is not a good practice
+
+
+
+const mobile_no = 123456789;
+let personName = "devloper";
+var personAddress = "Mumbai";
+personEmail = "abc@email.com";
+
+console.table ( [mobile_no , personName , personAddress , personEmail ,personAge]);
+//we use this method to print multiple variables
+
+let personAge ;
+//this undefined variable
+
+//done
---
+++ 02_datatypes/script.js
@@ -0,0 +1,54 @@
+
+//types of datatypes
+//string
+//boolean
+//number
+//bigInt  ,BigInt. An integer with arbitrary precision. For example: 9007199254740992n.
+//null
+//undefined
+//symbol ,. A data type whose instances are unique and immutable.
+
+//this are primitive datatypes.
+
+//object,  this is important datatype in javascript
+
+/* 
+
+Ensure that “use strict” is at the top
+
+"use strict";
+
+// this code works the modern way
+...
+
+
+*/
+
+let myName = "nandan";
+console.log(typeof myName);
+
+//type of myName is "string".
+//This is a sequence of text known as a string. To signify that the value is a string, enclose it in single or double quote marks.
+
+const myAge = 21;
+console.log(typeof myAge);
+
+//type of myAge is number.
+//This is a number. Numbers don't have quotes around them.
+
+let myAddress;
+console.log(typeof myAddress);
+
+//this is undefined datatype
+
+let myphone = null;
+console.log(typeof myphone);
+
+//type of null = object ,this is issue in javascript
+
+let myAns = true;
+console.log(typeof myAns);
+//This is a True/False value. The words true and false are special keywords that don't need quote marks.
+
+
+//done
